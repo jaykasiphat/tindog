78ad082cc2c4888e8d72c252c3deafc66d29cd06
jay <jaykasiphat@gmail.com>
2022-08-20 06:12:44 +0700

    Clean code

--- Dog.js
+++ Dog.js
@@ -3,18 +3,28 @@ class Dog {
     Object.assign(this, dog);
   }
 
-  updateMatchStatus(swiped, liked) {
-    this.hasBeenSwiped = swiped;
-    this.hasBeenLiked = liked;
+  updateMatchStatus(bool) {
+    this.hasBeenSwiped = true;
+    this.hasBeenLiked = bool;
   }
 
   getDogHtml() {
     const { name, avatar, age, bio } = this;
     return `
       <div class="profile-suggestion">
-        <img src="${avatar}" alt="${name}" class="dog">
+        <img src="${avatar}" alt="${name}" class="dog-img">
+        <img
+          src="./images/badge-like.png"
+          alt="accept badge"
+          class="accept-badge badge"
+        />
+        <img
+          src="./images/badge-nope.png"
+          alt="reject badge"
+          class="reject-badge badge"
+        />
         <div class="dog-info">
-          <h4>${name}, ${age}</h4>
+          <h3>${name}, ${age}</h3>
           <p>${bio}</p>
         </div>
       </div>
